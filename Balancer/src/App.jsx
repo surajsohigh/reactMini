d732a05892478f0e9e0d6a3e555cb72726955fd5
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2024-03-20 10:22:44 +0530

    Add files via upload

---
+++ Balancer/src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css';
+import Counter from './counter/Counter'
+
+function App() {
+
+  return (
+    <div className="App m-5">
+      <Counter></Counter>
+
+    </div>
+  );
+}
+
+export default App;
